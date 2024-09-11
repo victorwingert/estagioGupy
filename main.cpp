6d9d111f4eb1ab4cfa8aa93f9219d31cbb644ddb
Victor Wingert <victor.wingert@gmail.com>
2024-09-10 21:39:05 -0300

    questao 1 feita

--- main.cpp
+++ main.cpp
@@ -2,8 +2,34 @@
 
 using namespace std;
 
+bool pertenceFibonacci(int num){
+    int a = 0, b = 1, next = 0;
+
+    if(num == 0 || num == 1)
+        return true;
+
+    while(next < num){
+        next = a + b;
+        a = b;
+        b = next;
+    }
+
+    return (num == next);
+}
+
 int main()
 {
-    cout << "Hello world!" << endl;
+    //Q1
+    int n = 34;
+
+    if(pertenceFibonacci(n)) {
+        cout << "O numero " << n << " pertence a sequencia de Fibonacci" << endl;
+    } else {
+        cout << "O numero " << n << " nao pertence a sequencia de Fibonacci" << endl;
+    }
+
+
+
+
     return 0;
 }
